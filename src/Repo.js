adf0514d30a109dda451d43db47f9812138fe60c
reactoros <modmoney7@gmail.com>
2019-05-09 23:03:20 +0000

    put real data instead of placeholders
    putting the data recieved from the props into the card component to show real data from github api

--- src/Repo.js
+++ src/Repo.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
-//getting props from RepoList to assign them to the dom
+function CalculateInterval(creation){  
+
+    let today = new Date();
+    let diffc = creation.getTime() - today.getTime();  
+    return Math.round(Math.abs(diffc/(1000*60*60*24)));
+   }
+
+
 const Repo = (props) => {
 return (
 
   <div className="row mt-5">
     <div className="col-1">
-    <a target="blank" href=''><img alt='repo' src=''  className="img-fluid img-thumbnail mt-2" /></a>
+    <a target="blank" href={props.html_url}><img alt='repo' src={`${props.avatar_url}`}  className="img-fluid img-thumbnail mt-2" /></a>
     </div>
     <div  className="col-6 ml-5">
       <h3>{props.name}</h3>
       <p>{props.description}</p>
-      <label className="border border-dark p-2 mr-2">stars:154k</label> <label className="border border-dark p-2 mr-2">issues: 17</label><span>Submited 14 Days ago by <a target="blank" href={props.profile}>amin</a></span>
+      <label className="border border-dark p-2 mr-2">stars: {props.stargazers_count}</label> <label className="border border-dark p-2 mr-2">issues: {props.open_issues_count}</label><span>Submited {CalculateInterval(new Date(props.createAt))} Days ago by <a target="blank" href={props.profile}>{props.ownerName}</a></span>
     </div>
 </div>
 
